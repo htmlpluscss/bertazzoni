91d5294afd1646d4fb38e94ed0a18268d2e872e5
htmlpluscss <79198889134@ya.ru>
2020-05-17 21:51:06 +0300

    menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,84 +4,16 @@
 
 	// клик по гамбургеру
 
-	document.querySelector('.btn-menu-toggle').addEventListener('click', () => {
+	document.addEventListener('click', (e) => {
 
-		if(BERTAZZONI.OpenMenu) {
+		if(e.target.classList.contains('menu') || e.target.closest('.btn-menu-close')) {
 
 			document.body.classList.remove('menu-show');
 
-			window.scrollTo(0,BERTAZZONI.windowScrollOld);
-
-			BERTAZZONI.OpenMenu = false;
-
 		}
-		else {
-
-			BERTAZZONI.OpenMenu = true;
-
-			// записываем значение скролла страницы
-			BERTAZZONI.windowScrollOld = window.pageYOffset;
-			window.scrollTo(0,0);
+		else if (e.target.closest('.btn-menu-open')) {
 
 			document.body.classList.add('menu-show');
-
-		}
-
-	});
-
-	// субменю
-
-	var navArrowOpenSubMenu = document.querySelectorAll('.menu__arrow');
-
-	Array.prototype.forEach.call(navArrowOpenSubMenu, function(btn){
-
-		btn.addEventListener('click', () => {
-
-			btn.classList.toggle('menu__arrow--open');
-
-		});
-
-	});
-
-	// fixed
-
-	var ScrollTopPrev = window.pageYOffset;
-
-	PubSub.subscribe('windowScroll', ()=> {
-
-		if(!BERTAZZONI.menuOFF) {
-
-			if(!BERTAZZONI.OpenMenu) {
-
-				var top = window.pageYOffset;
-
-				if(top <= 0) {
-
-					document.body.classList.remove('header-fixed');
-
-				}
-				else if(top > window.innerHeight){
-
-					document.body.classList.toggle('header-fixed', top <= ScrollTopPrev);
-
-				}
-
-				setTimeout(()=>{
-
-					ScrollTopPrev = top;
-
-				},100);
-
-			}
-
-		}
-		else {
-
-			setTimeout(()=>{
-
-				ScrollTopPrev = window.pageYOffset;
-
-			},100);
 
 		}
 
