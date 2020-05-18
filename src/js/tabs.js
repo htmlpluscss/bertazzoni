fe5ec1a4d4cb6fe0c855a3c7bf54e298cdd69b46
htmlpluscss <79198889134@ya.ru>
2020-05-18 16:32:58 +0300

    Сопутствующие

---
+++ src/js/tabs.js
@@ -0,0 +1,49 @@
+((tabs)=>{
+
+	"use strict";
+
+	if(!tabs.length) {
+
+		return;
+
+	}
+
+	Array.prototype.forEach.call(tabs, (tab) => {
+
+		var btn = tab.querySelectorAll('.tabs__btn'),
+			item = tab.querySelectorAll('.tabs__item'),
+			nav = document.createElement('div');
+
+		Array.prototype.forEach.call(btn, (el,index) => {
+
+			nav.appendChild(el);
+
+			el.addEventListener('click', () => {
+
+				Array.prototype.forEach.call(btn, (e,i) => {
+
+					if(i == index) {
+
+						e.classList.add('tabs__btn--active');
+						item[i].classList.remove('visuallyhidden');
+
+					}
+					else{
+
+						e.classList.remove('tabs__btn--active');
+						item[i].classList.add('visuallyhidden');
+
+					}
+
+				});
+
+			});
+
+		});
+
+		nav.className = 'tabs__nav';
+		tab.insertBefore(nav, item[0]);
+
+	});
+
+})(document.querySelectorAll('.tabs'));
