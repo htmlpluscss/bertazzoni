9ebc17eee0e743b4a70b60635e832d7d2027b30f
htmlpluscss <79198889134@ya.ru>
2020-05-20 23:12:02 +0300

    slider about

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -22,7 +22,8 @@
 			swipePrev = document.createElement('button'),
 			items = swipe.querySelectorAll('.swiper-slide'),
 			count = items.length,
-			homeSlider = swipe.classList.contains('swiper-container--home-slider');
+			homeSlider = swipe.classList.contains('swiper-container--home-slider'),
+			homeAboutSlider = swipe.classList.contains('swiper-container--home-about');
 
 		swipeNav.className = 'swiper-pagination';
 		swipeControls.className = 'swiper-controls';
@@ -31,9 +32,9 @@
 		swipePrev.className = 'swiper-button-prev button';
 		swipeNext.className = 'swiper-button-next button';
 
-		swipePrev.innerHTML = '<svg width="63" height="63" viewBox="0 0 63 63"><path d="M20.1404 31.8388l7.1875 7.1875a.4792.4792 0 00.8179-.3388v-.9583a.4789.4789 0 00-.1404-.3388l-4.932-4.932h19.4474A.479.479 0 0043 31.9792v-.9583a.4789.4789 0 00-.4792-.4792H23.0734l4.932-4.9321a.4785.4785 0 00.1404-.3388v-.9583a.4788.4788 0 00-.4791-.4792.4791.4791 0 00-.3388.1404l-7.1875 7.1875a.479.479 0 000 .6776z"/></svg>';
+		swipePrev.innerHTML = '<svg width="23" height="17" viewBox="0 0 23 17"><path d="M.1404 8.8388l7.1875 7.1875a.4792.4792 0 00.818-.3388v-.9583a.4791.4791 0 00-.1405-.3388l-4.932-4.932h19.4474A.479.479 0 0023 8.9792v-.9583a.4789.4789 0 00-.4792-.4792H3.0734l4.932-4.932a.4788.4788 0 00.1404-.3389v-.9583a.4787.4787 0 00-.4791-.4792.479.479 0 00-.3388.1404L.1404 8.1612a.479.479 0 000 .6776z"/></svg>';
 
-		swipeNext.innerHTML = '<svg width="63" height="63" viewBox="0 0 63 63"><path d="M42.8596 31.8388l-7.1875 7.1875a.4792.4792 0 01-.8179-.3388v-.9583a.4789.4789 0 01.1404-.3388l4.932-4.932H20.4792A.479.479 0 0120 31.9792v-.9583a.4789.4789 0 01.4792-.4792h19.4474l-4.932-4.9321a.4785.4785 0 01-.1404-.3388v-.9583a.4788.4788 0 01.4791-.4792.4791.4791 0 01.3388.1404l7.1875 7.1875a.479.479 0 010 .6776z"/></svg>';
+		swipeNext.innerHTML = '<svg width="23" height="17" viewBox="0 0 23 17"><path d="M22.8596 8.8388l-7.1875 7.1875a.4792.4792 0 01-.8179-.3388v-.9583a.4789.4789 0 01.1404-.3388l4.932-4.932H.4792A.479.479 0 010 8.9792v-.9583a.479.479 0 01.4792-.4792h19.4474l-4.932-4.932a.4786.4786 0 01-.1404-.3389v-.9583a.4786.4786 0 01.4791-.4792.479.479 0 01.3388.1404l7.1875 7.1875a.479.479 0 010 .6776z"/></svg>';
 
 		swipeBtns.appendChild(swipePrev);
 		swipeBtns.appendChild(swipeNext);
@@ -75,6 +76,26 @@
 			}
 
 		}
+
+		if (homeAboutSlider) {
+
+			toggleSwipe = function() {
+
+				toggleSwipe = false;
+
+				new Swiper(swipe, {
+					loop: true,
+					slidesPerView: 'auto',
+					navigation: {
+						nextEl: swipeNext,
+						prevEl: swipePrev
+					}
+				});
+
+			}
+
+		}
+
 /*
 		if (slidesPerViewAuto) {
 
