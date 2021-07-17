9106eac80e1e51a4cf0f59d3660cfa4144fa74a7
htmlpluscss <79198889134@ya.ru>
2021-07-17 22:37:45 +0300

    header search

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -9,6 +9,16 @@
 
 	var input = form.querySelector('.header-search__input');
 
-	input.addEventListener('focus', ()=> form.classList.add('is-open'));
+	input.addEventListener('focus', ()=> document.body.classList.add('header-search-open'));
+
+	window.addEventListener('click', event => {
+
+		if(document.body.classList.contains('header-search-open') && !event.target.closest('.header-search')){
+
+			document.body.classList.remove('header-search-open');
+
+		}
+
+	});
 
 })(document.querySelector('.header-search'));
